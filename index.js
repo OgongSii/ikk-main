6be3a9c29a3d4d7a0c3e4456e87d79cbc8f6420d
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2022-10-18 17:39:16 +0900

    agfdlbhrdfgbifhbvgrfeibrfvedibrfeionerfnergonerfgonegrvetionrjonigj iotojnigrtiorggdggrtgtrggiourt

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("scroll", function() {
+document.addEventListener("scroll", function () {
     console.log(`scroll`);
 });
 
@@ -16,12 +16,60 @@ $(window).on("wheel", function (e) {
     if (e.originalEvent.deltaY > 0) {
         if (page == 4) return;
         page++;
-     
+
     } else if (e.originalEvent.deltaY < 0) {
         if (page == 1) return;
-        page--;  
+        page--;
     }
     var posTop = (page - 1) * $(window).height();
     mHtml.animate({ scrollTop: posTop });
-    
-})  
+
+})
+
+$(document).ready(function () {
+
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+
+                scrollTop: $(hash).offset().top
+
+            }, 800, function () {
+
+                window.location.hash = hash;
+
+            });
+
+        }
+
+    });
+
+});
+
+$(document).ready(function () {
+
+    $("a").on('click', function (event) {
+        // button1 클릭시 page를 1로 변경
+        if (this.hash == "#section1") {
+            page = 1;
+        }
+        // button2 클릭시 page를 2로 변경
+        else if (this.hash == "#section2") {
+            page = 2;
+        }
+        // button3 클릭시 page를 3로 변경
+        else if (this.hash == "#section3") {
+            page = 3;
+        }
+        // button4 클릭시 page를 4로 변경
+        else if (this.hash == "#section4") {
+            page = 4;
+        }
+    });
+});
